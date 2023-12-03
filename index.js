3fcb5804ffeceb117e345e01bfe3940d8f84f6bc
Raheel94 <muha.raheel94@gmail.com>
2023-12-03 11:27:36 +0500

    go

---
+++ index.js
@@ -0,0 +1,7 @@
+"use strict";
+// Exercise 2:  Write a program that takes input and calculates the volume of a cube.
+let a = 3;
+function volume(num1) {
+    console.log("The volume of a cube is", num1 ** a);
+}
+volume(10);
---
+++ index.ts
@@ -0,0 +1,6 @@
+// Exercise 2:  Write a program that takes input and calculates the volume of a cube.
+let a:number = 3
+function volume(num1: number){
+    console.log("The volume of a cube is",num1 ** a)
+}
+volume(10);
